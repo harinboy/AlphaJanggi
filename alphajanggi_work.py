a876cdc414524034e55698d9527887cb46ad4cf9
Harin Lee <harinboy@snu.ac.kr>
2023-03-20 22:30:12 +0900

    Faust 1st run

--- alphajanggi_work.py
+++ alphajanggi_work.py
@@ -1,6 +1,4 @@
-# %% [code]
-# %% [code]
-from alphajanggi_utils import *
+from AlphaJanggi_utils import *
 import torch
 import torch.multiprocessing as mp
 from numpy.random import shuffle
@@ -46,8 +44,7 @@ def TwoProcess(model_alpha, model_alpha2, iter_num, lr, n=16, think_time=1024, t
 def TwoProcess_notrain(model_alpha, model_alpha2, iter_num, n=16, think_time=1024):
     model_alpha.eval()
     model_alpha2.eval()
-    #total_samples = LoadSamples('/kaggle/input/alphajanggi-dataset/AlphaJanggi_samples')
-    #total_samples = LoadSamples('/kaggle/input/alphajanggi-samples/AlphaJanggi_samples')
+    total_samples = LoadSamples('./AlphaJanggi_samples2')
     with mp.Pool(2) as p:
         for i in range(iter_num):
             model_alpha2.load_state_dict(model_alpha.state_dict())
@@ -56,7 +53,7 @@ def TwoProcess_notrain(model_alpha, model_alpha2, iter_num, n=16, think_time=102
             samples+=samples2
             print(len(samples))
             #total_samples += samples
-            total_samples = samples
+            total_samples += samples
             if len(total_samples)>40000:
                 total_samples = total_samples[-40000:]
             print(len(total_samples))
@@ -64,27 +61,35 @@ def TwoProcess_notrain(model_alpha, model_alpha2, iter_num, n=16, think_time=102
     torch.save(model_alpha.state_dict(), 'AlphaJanggi.pth')
     SaveSamples(total_samples, 'AlphaJanggi_samples2')
 
+def EightProcess_notrain(model_alpha, iter_num, n=16, think_time=1024):
+    model_alpha.eval()
+    total_samples = LoadSamples('./AlphaJanggi_samples2')
+    with mp.Pool(8) as p:
+        for i in range(iter_num):
+            samples_list = p.starmap(Alpha_SelfPlay_Multi, [(model_alpha, n, think_time)]*8)
+            torch.cuda.empty_cache()
+            samples = []
+            for sample in samples_list:
+                samples+=sample
+            print(len(samples))
+            total_samples += samples
+            #total_samples = samples
+            if len(total_samples)>40000:
+                total_samples = total_samples[-40000:]
+            print(len(total_samples))
+    #torch.save(model_alpha.state_dict(), 'AlphaJanggi.pth')
+    SaveSamples(total_samples, 'AlphaJanggi_samples2')
+
 def main():
+    print(torch.cuda.device_count())
     torch.backends.cudnn.benchmark = True
-    if torch.cuda.device_count()>=2:
-        device0 = torch.device("cuda:0")
-        device1 = torch.device("cuda:1")
-        print("Two cuda")
-        model_alpha = AlphaJanggi().to(device0)
-        model_alpha.device=device0
-        #model_alpha.load_state_dict(torch.load('/kaggle/input/alphajanggi-dataset/AlphaJanggi.pth', map_location = device0))
-        model_alpha.load_state_dict(torch.load('/kaggle/input/alphajanggi-samples/AlphaJanggi.pth', map_location = device0))
-        model_alpha2 = AlphaJanggi().to(device1)
-        model_alpha2.device = device1
-        #TwoProcess(model_alpha, model_alpha2, 3, 3e-5, 32, 2048, 1, 32768, 256)
-        TwoProcess_notrain(model_alpha, model_alpha2, 1, 32, 4096)
-    else:
-        device = "cuda" if torch.cuda.is_available() else "cpu"
-        print(f"Using {device} device")
-        device = torch.device(device)
-        model_alpha = AlphaJanggi().to(device)
-        model_alpha.device=device
-        OneProcess(model_alpha, 3, 1e-2)
+    device0 = torch.device("cuda:0")
+    #print("Two cuda")
+    model_alpha = AlphaJanggi().to(device0)
+    model_alpha.device=device0
+    model_alpha.load_state_dict(torch.load('./AlphaJanggi_230313_2.pth', map_location = device0))
+    #TwoProcess(model_alpha, model_alpha2, 3, 3e-5, 32, 2048, 1, 32768, 256)
+    EightProcess_notrain(model_alpha, 1, 8, 4096)
 
 if __name__ == '__main__':
     mp.set_start_method('spawn')
